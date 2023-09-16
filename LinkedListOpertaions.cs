bb98a1014df6d8804e392372e2c08ab1422a6da2
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-16 19:07:54 +0530

    [sanjeeva] : added data into the linked list

---
+++ LinkedListOpertaions.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListAssignment
+{
+    internal class LinkedListOpertaions
+    {
+        public Node head;
+
+        public void addData(int data)
+        {
+            Node node = new Node(data);
+            if(this.head==null )
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp= head;
+                while(temp.Next!=null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine("{0} is inserted into the linked list",node.data);
+
+        }
+
+        public void DisplayData()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("linked list is empty");
+            }
+            else
+            {
+                Node tempNode = head;
+                while (tempNode != null)
+                {
+
+                    Console.WriteLine(tempNode.data + " ");
+                    tempNode = tempNode.Next;
+
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+
+namespace LinkedListAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListOpertaions list= new LinkedListOpertaions();
+            list.addData(56);
+            list.addData(30);
+            list.addData(70);
+            
+            
+
+            list.DisplayData();
+
+
+        }
+    }
+}
